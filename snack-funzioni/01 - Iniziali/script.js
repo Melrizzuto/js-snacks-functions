cf163b419047eaf3a1a1feb15bc6898f34e70cfc
Melrizzuto <melrizzuto4@gmail.com>
2024-11-06 14:21:56 +0100

    completed initial snack js

--- snack-funzioni/01 - Iniziali/script.js
+++ snack-funzioni/01 - Iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function initialFilter(arrayName){
+    let newArray = []; variabile di accumulo
+    for (let i = 0; i < arrayName.length; i++) {
+        const element = arrayName[i];
+        newArray.push(element[0]);
+        
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(initialFilter(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
