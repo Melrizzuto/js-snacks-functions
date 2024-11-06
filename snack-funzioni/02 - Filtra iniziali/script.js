a613ffcaa3de352a3e11ec54e1d41ea54354948c
Melrizzuto <melrizzuto4@gmail.com>
2024-11-06 16:44:50 +0100

    completed second ex js

--- snack-funzioni/02 - Filtra iniziali/script.js
+++ snack-funzioni/02 - Filtra iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+// Dichiara la funzione qui
+function letterInitialFilter(arrayStr, letter) {
+    let newArray = [];
+    const letterChoice = letter.toLowerCase();
+    for (let i = 0; i < arrayStr.length; i++) {
 
-// Dichiara la funzione qui.
+        if (arrayStr[i].charAt(0).toLowerCase() === letterChoice) {
+            newArray.push(arrayStr[i]);
+        }
+    }
 
+    
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(letterInitialFilter(names, "a"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
