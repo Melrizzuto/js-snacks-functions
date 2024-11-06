8f6d1fe61c2954f46e4c7367ff41ef8a07a6f5e7
Melrizzuto <melrizzuto4@gmail.com>
2024-11-06 17:08:26 +0100

    completed third ex js

--- snack-funzioni/03 - conta vocali/script.js
+++ snack-funzioni/03 - conta vocali/script.js
@@ -4,10 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function filterVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let vowelsArray = [];
 
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i].toLowerCase())) {
+            vowelsArray.push(str[i].toLowerCase());
+        }
+    }
+
+    return vowelsArray.length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filterVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
